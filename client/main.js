91d6017fedfd0730bcc3d8ac476c3d30a2d40c96
hudson <archreth@gmail.com>
2017-12-10 19:59:52 -0800

    working all graphs, no text yet

--- client/main.js
+++ client/main.js
@@ -135,22 +135,124 @@ function drawTypeLine(type){
     var rightArrow;
 
     switch(type){
+        //Sanguine
         case "ISTJ":
             typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(400,200));
             leftArrow = new paper.Path.Line(new paper.Point(400, 200), new paper.Point(400,210));
             rightArrow = new paper.Path.Line(new paper.Point(400, 200), new paper.Point(400,210));
             leftArrow.rotate(45, new paper.Point(400, 200));
-            rightArrow.rotate(95, new paper.Point(400, 200))
+            rightArrow.rotate(95, new paper.Point(400, 200));
             break;
         case "ESTJ":
             typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(450,150));
             leftArrow = new paper.Path.Line(new paper.Point(450, 150), new paper.Point(450,160));
             rightArrow = new paper.Path.Line(new paper.Point(450, 150), new paper.Point(450,160));
             leftArrow.rotate(45, new paper.Point(450, 150));
-            rightArrow.rotate(95, new paper.Point(450, 150))
+            rightArrow.rotate(95, new paper.Point(450, 150));
+            break;
+        case "ENFP":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(350, 50));
+            leftArrow = new paper.Path.Line(new paper.Point(350, 50), new paper.Point(350, 60));
+            rightArrow = new paper.Path.Line(new paper.Point(350, 50), new paper.Point(350, 60));
+            leftArrow.rotate(1, new paper.Point(350, 50));
+            rightArrow.rotate(55, new paper.Point(350, 50));
+            break;
+        case "INFP":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(300, 100));
+            leftArrow = new paper.Path.Line(new paper.Point(300, 100), new paper.Point(300, 110));
+            rightArrow = new paper.Path.Line(new paper.Point(300, 100), new paper.Point(300, 110));
+            leftArrow.rotate(-15, new paper.Point(300, 100));
+            rightArrow.rotate(55, new paper.Point(300, 100));
+            break;
+        //Phlegmatics            
+        case "ISFJ":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(150, 200));
+            leftArrow = new paper.Path.Line(new paper.Point(150, 200), new paper.Point(150, 210));
+            rightArrow = new paper.Path.Line(new paper.Point(150, 200), new paper.Point(150, 210));
+            leftArrow.rotate(-85, new paper.Point(150, 200));
+            rightArrow.rotate(-15, new paper.Point(150, 200));
+            break;
+        case "ESFJ":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(200, 150));
+            leftArrow = new paper.Path.Line(new paper.Point(200, 150), new paper.Point(200, 160));
+            rightArrow = new paper.Path.Line(new paper.Point(200, 150), new paper.Point(200, 160));
+            leftArrow.rotate(-75, new paper.Point(200, 150));
+            rightArrow.rotate(15, new paper.Point(200, 150));
+            break;
+        case "ENTP":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(100, 50));
+            leftArrow = new paper.Path.Line(new paper.Point(100, 50), new paper.Point(100, 60));
+            rightArrow = new paper.Path.Line(new paper.Point(100, 50), new paper.Point(100, 60));
+            leftArrow.rotate(-5, new paper.Point(100, 50));
+            rightArrow.rotate(-55, new paper.Point(100, 50));
+            break;
+        case "INTP":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(50, 100));
+            leftArrow = new paper.Path.Line(new paper.Point(50, 100), new paper.Point(50, 110));
+            rightArrow = new paper.Path.Line(new paper.Point(50, 100), new paper.Point(50, 110));
+            leftArrow.rotate(-85, new paper.Point(50, 100));
+            rightArrow.rotate(-15, new paper.Point(50, 100));
             break;
 
+        //Melancholics
+        case "ESTP":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(100, 300));
+            leftArrow = new paper.Path.Line(new paper.Point(100, 300), new paper.Point(100, 310));
+            rightArrow = new paper.Path.Line(new paper.Point(100, 300), new paper.Point(100, 310));
+            leftArrow.rotate(-85, new paper.Point(100, 300));
+            rightArrow.rotate(-145, new paper.Point(100, 300));
+            break;
+        case "ISTP":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(50, 350));
+            leftArrow = new paper.Path.Line(new paper.Point(50, 350), new paper.Point(50, 360));
+            rightArrow = new paper.Path.Line(new paper.Point(50, 350), new paper.Point(50, 360));
+            leftArrow.rotate(-75, new paper.Point(50, 350));
+            rightArrow.rotate(-140, new paper.Point(50, 350));
+            break;
+        case "ENFJ":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(200, 400));
+            leftArrow = new paper.Path.Line(new paper.Point(200, 400), new paper.Point(200, 410));
+            rightArrow = new paper.Path.Line(new paper.Point(200, 400), new paper.Point(200, 410));
+            leftArrow.rotate(-115, new paper.Point(200, 400));
+            rightArrow.rotate(-190, new paper.Point(200, 400));
+            break;
+        case "INFJ":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(150, 450));
+            leftArrow = new paper.Path.Line(new paper.Point(150, 450), new paper.Point(150, 460));
+            rightArrow = new paper.Path.Line(new paper.Point(150, 450), new paper.Point(150, 460));
+            leftArrow.rotate(-110, new paper.Point(150, 450));
+            rightArrow.rotate(-195, new paper.Point(150, 450));
+            break;
 
+        //Cholerics
+        case "ESFP":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(350, 300));
+            leftArrow = new paper.Path.Line(new paper.Point(350, 300), new paper.Point(350, 310));
+            rightArrow = new paper.Path.Line(new paper.Point(350, 300), new paper.Point(350, 310));
+            leftArrow.rotate(85, new paper.Point(350, 300));
+            rightArrow.rotate(165, new paper.Point(350, 300));
+            break;
+        case "ISFP":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(300, 350));
+            leftArrow = new paper.Path.Line(new paper.Point(300, 350), new paper.Point(300, 360));
+            rightArrow = new paper.Path.Line(new paper.Point(300, 350), new paper.Point(300, 360));
+            leftArrow.rotate(-165, new paper.Point(300, 350));
+            rightArrow.rotate(105, new paper.Point(300, 350));
+            break;
+        case "ENTJ":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(450, 400));
+            leftArrow = new paper.Path.Line(new paper.Point(450, 400), new paper.Point(450, 410));
+            rightArrow = new paper.Path.Line(new paper.Point(450, 400), new paper.Point(450, 410));
+            leftArrow.rotate(85, new paper.Point(450, 400));
+            rightArrow.rotate(165, new paper.Point(450, 400));
+            break;
+        case "INTJ":
+            typeLine = new paper.Path.Line(new paper.Point(250, 250), new paper.Point(400, 450));
+            leftArrow = new paper.Path.Line(new paper.Point(400, 450), new paper.Point(400, 460));
+            rightArrow = new paper.Path.Line(new paper.Point(400, 450), new paper.Point(400, 460));
+            leftArrow.rotate(180, new paper.Point(400, 450));
+            rightArrow.rotate(90, new paper.Point(400, 450));
+            break;
 
         default:
             return;
